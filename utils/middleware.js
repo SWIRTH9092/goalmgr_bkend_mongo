8425f31d0b1f858d59e8092cc37ad89e39c6e43e
Your Name <you@example.com>
2023-03-23 08:43:36 -0400

    update middleware for morgan and cors

--- utils/middleware.js
+++ utils/middleware.js
@@ -4,13 +4,17 @@
 
 require("dotenv").config();
 const express = require('express');
+const morgan = require('morgan');  //required for logging
+const cors = require('cors');  //required for cors
 
 //****************************************
 //   middleware
 //****************************************
 
 const middleware =(app) => {
-    app.use(express.json())
+    app.use(express.json());
+    app.use(morgan("dev"));
+    app.use(cors());
 }
 
 //****************************************
