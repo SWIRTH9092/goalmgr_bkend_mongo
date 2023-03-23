6a77a6a5f7b45a23d77e7a3af1b1eef586e96174
Your Name <you@example.com>
2023-03-23 09:34:30 -0400

    add user.js - mongo user schema

---
+++ models/user.js
@@ -0,0 +1,37 @@
+//****************************************
+//   import dependencies
+//****************************************
+
+const { stringify } = require("querystring");
+const mongoose = require("./connection");
+
+//****************************************
+//   Define User Model
+//****************************************
+
+const {Schema, model } = mongoose;
+
+//****************************************
+//   User Schema
+//****************************************
+
+const userSchema = new Schema ({
+    userid: {type: String},
+    password: {type: String},
+    FirstName: {type: String},
+    LastName: {type: String},
+    Email: {type: String},
+    CellPhoneNumber: {type: String} 
+});
+
+//****************************************
+//   make user Model
+//****************************************
+
+const User = model("User", userSchema);
+
+//****************************************
+//  Export User Model
+//****************************************
+
+module.exports = User
