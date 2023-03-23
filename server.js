c14a92a966de47e2adf71b2d751855da5e67ff1e
Your Name <you@example.com>
2023-03-23 07:52:33 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,30 @@
+//****************************************
+//   Dependencies
+//****************************************
+
+require ("dotenv").config();
+
+// get .env variables
+
+const { PORT = 3001 } = process.env;
+
+// import express
+const express = require("express");
+
+//create application object
+const app = express();
+
+//****************************************
+//   Routes
+//****************************************
+
+//create a test route
+app.get ("/", (req, res) => {
+    res.send("hello world");
+});
+
+//****************************************
+//   Listener
+//****************************************
+
+app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
