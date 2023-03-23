1696207ef0a5d661fc76cb538e4608ba89a6a81f
Your Name <you@example.com>
2023-03-23 19:54:13 -0400

    add userPost Route

--- controllers/user/userPost.js
+++ controllers/user/userPost.js
@@ -1,25 +1,22 @@
 //----------------------------
 //  Import dependencies
 //----------------------------
-// const express = require("express")
+const express = require("express")
 
 // for MongoDB database
-// const Bookmark = require("../../models/bookmark")
-
-// const bookmarkPost = async (req, res) => {
-//     try {
-//         //  create bookmark
-//         res.json (await Bookmark.create(req.body));
-//     } catch (error) {
-//         //send error
-//         res.status(400).json(error)
-//     }
-    
-//     //res.send("post bookmark controller")
-// };
+const User = require("../../models/user")
+const userPost = async (req, res) => {
+    try {
+        //  create user
+        res.json (await User.create(req.body));
+    } catch (error) {
+        //send error
+        res.status(400).json(error)
+    }
+};
 
 //----------------------------
 //  Export controller function
 //----------------------------
 
-module.exports = userPost
+module.exports = userPost;
--- routers/user.js
+++ routers/user.js
@@ -7,7 +7,7 @@ const express = require('express') // bring this in so we can make our router
 // controller dependencies
 //-------------------------------------------
 const userIndex = require ("../controllers/user/userIndex")
-// const userPost = require("../controllers/user/userPost")
+const userPost = require("../controllers/user/userPost")
 // const userUpdate = require("../controllers/user/userUpdate")
 // const userDelete = require("../controllers/user/userDelete")
 // const userShow = require("../controllers/user/userShow")
@@ -21,7 +21,7 @@ const router = express.Router()
 // routers
 //-------------------------------------------
 router.get('/', userIndex);  // get all 
-// router.post('/', userPost);  // Create New 
+router.post('/', userPost);  // Create New 
 // router.put('/:id', userUpdate); //Update Existing
 // router.delete('/:id', userDelete)  // delete Existing
 // router.get('/:id', userShow)  // Show a specific item
