82cb3a844f1cd55547da49c54b9435e59b2fb10b
Your Name <you@example.com>
2023-03-23 20:03:17 -0400

    add show route

--- routers/user.js
+++ routers/user.js
@@ -10,7 +10,7 @@ const userIndex = require ("../controllers/user/userIndex")
 const userPost = require("../controllers/user/userPost")
 // const userUpdate = require("../controllers/user/userUpdate")
 // const userDelete = require("../controllers/user/userDelete")
-// const userShow = require("../controllers/user/userShow")
+const userShow = require("../controllers/user/userShow")
 
 //-------------------------------------------
 // create Route
@@ -24,7 +24,7 @@ router.get('/', userIndex);  // get all
 router.post('/', userPost);  // Create New 
 // router.put('/:id', userUpdate); //Update Existing
 // router.delete('/:id', userDelete)  // delete Existing
-// router.get('/:id', userShow)  // Show a specific item
+router.get('/:id', userShow)  // Show a specific item
 
 //-------------------------------------------
 // Export the Router
