ce339ab1afdf423641957d3aaaf74edc78f2e1a1
Your Name <you@example.com>
2023-03-29 08:15:15 -0400

    update user test data

--- models/seed.js
+++ models/seed.js
@@ -14,14 +14,32 @@ const Goal = require('./goal')
 mongoose.connection.on('open', () => {
     // define user data to put in the table
     const startingUser = [
-        { 
-            u_Userid: "minnie50",
-            u_Password: "123456789",
-            u_FirstName: "Minnie",
-            u_LastName: "Mouse",
-            u_Email: "minnie@disney.org",
-            u_CellPhoneNumber: "1112223333"
+        {
+            "u_Userid": "Minnie1940",
+            "u_RootKey": "DT2023325183944-$2a$10$nEwYPyPJC1QNTq3qh.E11uaD.lHb7hzqdafs0hx60pLw35UH8ETlO",
+            "u_Password": "MickeyMouse",
+            "u_FirstName": "Minnie",
+            "u_LastName": "Mouse",
+            "u_Email": "Minnie@disney.org",
+            "u_CellPhoneNumber": "1112223333"
+        },
+        {
+            "u_Userid": "Mickey1935",
+            "u_Password": "MinnieMouse",
+            "u_FirstName": "Mickey",
+            "u_RootKey": "DT2023325184246-$2a$10$MAfL8fvCgmrRTpsxkIEbaOB9gEJikbHxxn3/rBHR5WCzB3NbsjZSyUH8ETlO",
+            "u_LastName": "Mouse",
+            "u_Email": "Mickey@disney.org",
+            "u_CellPhoneNumber": "7778889999"
         }
+        // { 
+        //     u_Userid: "minnie50",
+        //     u_Password: "123456789",
+        //     u_FirstName: "Minnie",
+        //     u_LastName: "Mouse",
+        //     u_Email: "minnie@disney.org",
+        //     u_CellPhoneNumber: "1112223333"
+        // }
     ]
 
     const startingGoal = [
@@ -43,7 +61,7 @@ mongoose.connection.on('open', () => {
     //         mongoose.connection.close();
     //     })
     
-       Goal.create(startingGoal)
+       User.create(startingUser)
         .then (result => {
             mongoose.connection.close();
         })
