efe70d0532c1b11dc064e17a8e494af58c984523
Your Name <you@example.com>
2023-03-29 09:02:17 -0400

    fix userlogin for u_RootKey

--- controllers/goal/goalIndex.js
+++ controllers/goal/goalIndex.js
@@ -1,15 +1,16 @@
 const express = require("express")
 
 const Goal = "../../models/goal"
-const isLoggedIn = "../../utils/isLoggedIn.js"
+// const isLoggedIn = "../../utils/isLoggedIn.js"
 
   const goalIndex = async (req, res) => {
         try {
-            if(isLoggedIn) {
-                const u_RootKey = req.payload.u_RootKey;
-                const goals = await Goal.find({ u_RootKey });
-                res.json(goals);               
-            } 
+            // if(isLoggedIn) {
+            //     const u_RootKey = req.payload.u_RootKey;
+                console.log("in index route")
+                const goals = await Goal.find('DT2023325183944-$2a$10$nEwYPyPJC1QNTq3qh.E11uaD.lHb7hzqdafs0hx60pLw35UH8ETlO');
+                res.json(goals); 
+            //}              
         }
         catch(error) {
             res.status(400).json({error});
--- controllers/user/userLogin.js
+++ controllers/user/userLogin.js
@@ -12,14 +12,20 @@ const User = require("../../models/user")
 
 const userLogin = async (req, res) => {
     try {
+        console.count("inLogin")
         const { u_Userid, u_Password } = req.body;
         //get the user
+        console.count("inLogin")
         const user = await User.findOne({ u_Userid })
+        console.count("inLogin")
         if (user) {
+            console.count("inLogin")
             const passwordCheck = await bcrypt.compare(u_Password, user.u_Password)
             if (passwordCheck) {
+                const u_RootKey = user.u_RootKey
                 res.json({u_RootKey, status: "Logged In"})
             } else {
+
                 res.status(400).json({error: "Password does not match"})               
             }
             
