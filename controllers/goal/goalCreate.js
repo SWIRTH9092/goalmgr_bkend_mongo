50bba9a06fbbabe4e8cee6abb23af7184adcd414
Your Name <you@example.com>
2023-03-28 22:07:51 -0400

    comment out check for isLoggedIn

--- controllers/goal/goalCreate.js
+++ controllers/goal/goalCreate.js
@@ -5,14 +5,14 @@ const isLoggedIn = "../../utils/isLoggedIn.js"
 
   const goalCreate = async (req, res) => {
         try {
-            if(isLoggedIn()) {
-                const g_RootKey = req.payload.g_RootKey;
-                req.body.g_U_Rootkey = g_RootKey;
+            // if(isLoggedIn()) {
+                // const g_RootKey = req.payload.g_RootKey;
+                // req.body.g_U_Rootkey = g_RootKey;
                 console.log(req.body)
                 res.json(await Goal.create(req.body));
-            }  else {
-                res.status(400).json("error: not Logged in")
-            }
+            // }  else {
+            //     res.status(400).json("error: not Logged in")
+            // }
                      
         }
         catch(error) {
