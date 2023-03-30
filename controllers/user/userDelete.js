94294c657a279f7bbd1f5aae40020e3507b476f1
Your Name <you@example.com>
2023-03-30 16:53:02 -0400

    remove console.log from userDelete

--- controllers/user/userDelete.js
+++ controllers/user/userDelete.js
@@ -9,15 +9,12 @@ const User = require("../../models/user")
 const userDelete = async (req, res) => {
     try {
         // send user to delete
-        console.count("in delete")
         const userid = req.payload.u_Userid;
-        console.count("in delete")
         if (req.body.u_Userid === userid) {
             res.json(await User.findByIdAndRemove(req.params.id))  
             res
                 .clearCookie("token")
                 .json({ response: "Your userid id has been deleted" });  
-            console.count("in delete") 
         } else {
             res.status(400).json("error: Username mismatch");            
         }
